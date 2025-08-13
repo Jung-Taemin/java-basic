e0fb2560dfbc180f931caeb2e70c18659f6da6f3
taemin <jungtaemin1214@naver.com>
2025-08-13 22:14:34 +0900

    change

--- src/access/a/AccessInnerMain.java
+++ src/access/a/AccessInnerMain.java
@@ -1,4 +1,10 @@
 package access.a;
 
 public class AccessInnerMain {
+    public static void main(String[] args) {
+        AccessData data = new AccessData();
+        //public 호출 가능
+        data.publicField = 1;
+        data.publicMethod();
+    }
 }
