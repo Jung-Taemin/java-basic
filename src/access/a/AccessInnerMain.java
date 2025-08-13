f5f7ebda292e8576d2c0dff4bcba974a036a678e
taemin <jungtaemin1214@naver.com>
2025-08-14 00:38:30 +0900

    change

--- src/access/a/AccessInnerMain.java
+++ src/access/a/AccessInnerMain.java
@@ -6,5 +6,12 @@ public class AccessInnerMain {
         //public 호출 가능
         data.publicField = 1;
         data.publicMethod();
+
+        //같은 패키지 default 호출 가능
+        data.defaultField = 2;
+        data.defaultMethod();
+
+        //private 호출 불가
+        data.privateField = 3;
     }
 }
